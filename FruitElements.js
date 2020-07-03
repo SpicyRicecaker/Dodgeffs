8b45160695c4b5bae0fce7f60b2ceb4bb580dc25
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-07-02 23:28:25 -0700

    Tried to define game states... SCUFFED

--- FruitElements.js
+++ FruitElements.js
@@ -21,14 +21,16 @@ function init() {
   player = new Player(playerId);
   entityList.push(player);
   //Pushes new enemy onto array
-  for(var i = 0; i < 2; ++i){
+  for (var i = 0; i < 2; ++i) {
     entityList.push(new Enemy(enemyId));
   }
+  //sets game state 
+  renderSubject = gameRender;
 }
 
-function resizeCanvas(){
-  width = (canvas.width = window.innerWidth);
-  height = (canvas.height = window.innerHeight);
+function resizeCanvas() {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
 }
 /*
 ctx.fillStyle = 'rgb(255, 0, 0)';
@@ -55,6 +57,7 @@ var oldTimeStamp = 0;
 var secondsPassed;
 var secondsToSpawn = 0;
 var running = true;
+var renderSubject;
 
 function game(timeStamp) {
   //timeStamp - oldTimeStamp should be around 1000/60, resulting in 1/60 in seconds passed
@@ -66,41 +69,49 @@ function game(timeStamp) {
   //Update game time
   totalGameTime += secondsPassed;
   secondsToSpawn += secondsPassed;
-  //Actual game 
+  //Actual game
   tick();
   render();
   //Recursively call the next frame
-  if(running){
+  if (running) {
     requestAnimationFrame(game);
   }
 }
 
-function gameOver(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //Draw background
-    ctx.beginPath();
-    ctx.fillStyle = "#434343";
-    ctx.fillRect(0, 0, width, height);
-    ctx.closePath();
-    ctx.beginPath();
-    ctx.fillStyle = "#f3f3f3";
-    ctx.font = "200px arial";
-    ctx.fillText("GAME OVER :D", width/2-700, height/2);
-    ctx.closePath();
+function gameOver() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //Draw background
+  ctx.beginPath();
+  ctx.fillStyle = "#434343";
+  ctx.fillRect(0, 0, width, height);
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.fillStyle = "#f3f3f3";
+  ctx.font = "200px arial";
+  ctx.fillText("GAME OVER :D", width / 2 - 700, height / 2);
+  ctx.closePath();
 }
 
 function tick() {
-  for(var i = 0; i < entityList.length; ++i){
+  for (var i = 0; i < entityList.length; ++i) {
     entityList[i].tick();
   }
-  if(secondsToSpawn > 5){
+  if (secondsToSpawn > 5) {
     entityList.push(new Enemy(enemyId));
     secondsToSpawn = 0;
   }
 }
 
 function render() {
-  if(!running){
+    renderSubject();
+}
+
+
+
+function titleRender() {}
+
+function gameRender() {
+  if (!running) {
     gameOver();
     return;
   }
@@ -123,27 +134,31 @@ function render() {
   ctx.beginPath();
   ctx.fillStyle = "#f3f3f3";
   ctx.font = "48px arial";
-  ctx.fillText((padNumber(Math.floor(totalGameTime/60)) + ":" + padNumber(Math.floor((totalGameTime%60)))), width/2-60, 50);
+  ctx.fillText(
+    padNumber(Math.floor(totalGameTime / 60)) +
+      ":" +
+      padNumber(Math.floor(totalGameTime % 60)),
+    width / 2 - 60,
+    50
+  );
   ctx.closePath();
   //Render entities
-  for(var i = 0; i < entityList.length; ++i){
+  for (var i = 0; i < entityList.length; ++i) {
     entityList[i].render();
   }
 }
+
+function endRender() {}
 
 function degToRad(degrees) {
   return (Math.PI / 180) * degrees;
 }
 
-function padNumber(n){
-  return (n < 10 ? '0':'') + n;
+function padNumber(n) {
+  return (n < 10 ? "0" : "") + n;
 }
 
 function onMouseClick(e) {
-  //debug
-  //player.setX(e.clientX);
-  //player.setY(e.clientY);
-  //end
   player.setDestinationX(e.clientX);
   player.setDestinationY(e.clientY);
   player.setMoving(false);
